504b5be708cd4e80e481c8d4fe218d2489950fe0
alevel <alevel@DESKTOP-B7L2L3R>
2021-08-06 21:08:52 +0300

    Первая часть задания готова

--- Modul3HW4/Modul3HW4/Modul3HW4/Class1.cs
+++ Modul3HW4/Modul3HW4/Modul3HW4/Class1.cs
@@ -8,5 +8,22 @@ namespace Modul3HW4
 {
     public class Class1
     {
+        public Func<int, int, int> SumDelHandler { get; set; }
+
+        public int SumOfSums(int a, int b)
+        {
+            var result = 0;
+            var sumDelList = SumDelHandler.GetInvocationList();
+            foreach (var item in sumDelList)
+            {
+                if (item != null)
+                {
+                    var test = item as Func<int, int, int>;
+                    result += test.Invoke(a, b);
+                }
+            }
+
+            return result;
+        }
     }
 }
--- Modul3HW4/Modul3HW4/Modul3HW4/Starter.cs
+++ Modul3HW4/Modul3HW4/Modul3HW4/Starter.cs
@@ -8,14 +8,31 @@ namespace Modul3HW4
 {
     public class Starter
     {
+        private int _res;
         public void Run()
         {
-            Console.WriteLine();
+            var class1 = new Class1();
+            class1.SumDelHandler += Sum;
+            class1.SumDelHandler += Sum;
+            MyTryCach(() => _res = class1.SumOfSums(2, 2));
+            Console.WriteLine(_res);
         }
 
         public int Sum(int a, int b)
         {
             return a + b;
+        }
+
+        public void MyTryCach(Action del)
+        {
+            try
+            {
+                del.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
