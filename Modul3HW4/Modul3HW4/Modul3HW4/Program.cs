e4388944fc18eedd50580b6c9df1d43ebb2f4252
alevel <alevel@DESKTOP-B7L2L3R>
2021-08-06 19:54:09 +0300

    Пустой прект

---
+++ Modul3HW4/Modul3HW4/Modul3HW4/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Modul3HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
---
+++ Modul3HW4/Modul3HW4/Modul3HW4/Starter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW4
+{
+    public class Starter
+    {
+        public void Run()
+        {
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
